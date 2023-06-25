be566c09f03397b32005bbce55f43c2b5fdde999
Aum Kansara <aumkan23@gmail.com>
2023-06-25 20:06:52 +0530

    added Switch Case Example

---
+++ SwitchCases.java
@@ -0,0 +1,11 @@
+public class SwitchCases{
+    public static void main(String args[]){
+        String name="Aum Kansara";
+        switch(name){
+            case "Name1" -> System.out.println("Hello Name1");
+            case "Name2" -> System.out.println("Hello Name1");
+            case "Aum Kansara" -> System.out.println("Hello Aum Kansara");
+            case "Name4" -> System.out.println("Hello Name1");
+        }
+    }
+}
