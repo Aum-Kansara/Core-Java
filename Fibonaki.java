c805c90ca091da209238ad7c86aef827ab72ee4e
Aum Kansara <aumkan23@gmail.com>
2023-06-25 19:51:13 +0530

    renamed file

--- Fibonaki.java
+++ Fibonaki.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main{
+public class Fibonaki{
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
         int num1=obj.nextInt();
